9914e92b98af444d35f9b6228c353894902aa95a
zoukang1993 <zoukang1993@163.com>
2019-02-20 13:50:11 +0800

    decorator resolve stage 1

--- src/components/AsyncComponent/index.js
+++ src/components/AsyncComponent/index.js
@@ -3,33 +3,33 @@ import {observer, inject} from 'mobx-react';
 import {computed} from 'mobx';
 
 export default function asyncComponent(importComponent, hasHeader = true) {
-    @inject('stores')
+    // @inject('stores')
     @observer class AsyncComponent extends React.Component {
-    constructor(props) {
-      super(props);
+        constructor(props) {
+            super(props);
 
-      this.state = {
-        component: null
-      };
+            this.state = {
+                component: null
+            };
+        }
+
+        // @computed get userStore() {
+        //     return this.props.stores.userStore;
+        // }
+
+        async componentDidMount() {
+            const {default: component} = await importComponent();
+            // await this.userStore.setHeader(hasHeader);
+
+            this.setState({component});
+        }
+
+        render() {
+            const C = this.state.component;
+
+            return C ? <C {...this.props}/> : <div className="m-lg text-center">加载中</div>;
+        }
     }
 
-        @computed get userStore() {
-      return this.props.stores.userStore;
-    }
-
-    async componentDidMount() {
-      const {default: component} = await importComponent();
-      await this.userStore.setHeader(hasHeader);
-
-      this.setState({component});
-    }
-
-    render() {
-      const C = this.state.component;
-
-      return C ? <C {...this.props}/> : <div className="m-lg text-center">加载中</div>;
-    }
-  }
-
-  return AsyncComponent;
+    return AsyncComponent;
 }
